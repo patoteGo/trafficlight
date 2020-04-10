1b5ac34eddeee107cf3c015f7a4320bb8293c409
patodekaz <pato@dekaz.cl>
2020-04-10 11:47:59 -0400

    Updates

---
+++ precache-manifest.dbe44096811e6145bb569bbb6e99f148.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d35955dcc80784bb024f0873149f7524",
+    "url": "/trafficlight/index.html"
+  },
+  {
+    "revision": "07feb7a21371e65a5123",
+    "url": "/trafficlight/static/css/main.c6352c47.chunk.css"
+  },
+  {
+    "revision": "fa7f837bf3c43dad7e7a",
+    "url": "/trafficlight/static/js/2.f5001293.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/trafficlight/static/js/2.f5001293.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07feb7a21371e65a5123",
+    "url": "/trafficlight/static/js/main.d6260b1e.chunk.js"
+  },
+  {
+    "revision": "07e23670bd51d3cd7847",
+    "url": "/trafficlight/static/js/runtime-main.fa982439.js"
+  }
+]);
